0ea6c9e73ffdbb6c99f0b83af58cd342b83772a6
Violet Death <ladymariaownsbro@gmail.com>
2020-06-22 20:18:55 -0500

    welp

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const htmlroutes = require("./Routes/htmlroutes")
 
 const app = express()
 
-const port = process.env.port || 9001
+const PORT = process.env.PORT || 9001;
 
 
 app.use(express.json())
@@ -13,8 +13,8 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
-app.use("/api", apiroutes )
-app.use("/", htmlroutes)
-app.listen(port, ()=> console.log(
-    "Listening on Port" + port
+app.use("./Routes/apiroutes", apiroutes )
+app.use("./Routes/htmlroutes", htmlroutes)
+app.listen(PORT, ()=> console.log(
+    "Listening on Port" + PORT
 ))
